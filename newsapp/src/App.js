d0c1d38484f03d6dbaf4ca117fa42fbcb72d9c20
NegiSenpai <abhayng12804@gmail.com>
2024-04-12 19:33:55 +0530

    updates

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
-        <News/>
+        <div className='container my-3'>
+          <h2>Top-Headlines</h2>
+        <div className="row">
+        <div className='col-md-4'>
+          <NewsItem title="My title" description="My desc"/>
+        </div>
+        <div className='col-md-4'>
+          <NewsItem title="My title" description="My desc"/>
+        </div>
+        <div className='col-md-4'>
+          <NewsItem title="My title" description="My desc"/>
+        </div>
+        </div>
+        </div>
       </div>
     );
   }
